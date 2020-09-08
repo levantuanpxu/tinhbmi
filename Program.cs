72829470ee5c6de48a897a2cab611fbc5574a071
levantuanpxu <70925631+levantuanpxu@users.noreply.github.com>
2020-09-08 22:34:08 +0700

    tinhbmi
    tinhbmi

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace tinhbmi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.OutputEncoding = System.Text.Encoding.UTF8;
+			double height;
+			double weight;
+			double bmi;
+			//Chiều cao: cm
+			//Cân nặng: kg
+			Console.WriteLine("Nhập chiều cao : ");
+			height = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Nhập cân nặng : ");
+			weight = Convert.ToDouble(Console.ReadLine());
+			height = height / 100;
+			bmi = weight / (height * height);
+			Console.WriteLine("BMI của bạn là:{0}", Math.Round(bmi, 4));
+			if (bmi < 18.5)
+				Console.WriteLine("Underweight.");
+			else if (bmi >= 18.5 && bmi < 25)
+				Console.WriteLine("Normal");
+			else if (bmi >= 25 && bmi < 30)
+				Console.WriteLine("Overweight");
+			else if (bmi >= 30)
+				Console.WriteLine("Obese");
+
+		}
+	}
+}
